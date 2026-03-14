5eab6af758304d257f9ed9d66dd9a97c62cbe861
Alberto Tarabella <alberto.tarabella04@gmail.com>
2026-03-14 15:08:35 +0100

    Add Audio test and AI Test

--- GamelabSupermarket/Source/GamelabSupermarket/Project/Gameplay/Slot.cpp
+++ GamelabSupermarket/Source/GamelabSupermarket/Project/Gameplay/Slot.cpp
@@ -1,29 +1,37 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Project/Gameplay/Slot.h"
 
-// Sets default values
 ASlot::ASlot()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
-// Called when the game starts or when spawned
 void ASlot::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
-
-
-// Called every frame
 void ASlot::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
+// ===== ADD TAG =====
+
+void ASlot::AddSlotTag(FGameplayTag Tag)
+{
+	GameplayTags.AddTag(Tag);
+}
+
+// ===== REMOVE TAG =====
+
+void ASlot::RemoveSlotTag(FGameplayTag Tag)
+{
+	GameplayTags.RemoveTag(Tag);
+}
+
+// ===== EQS INTERFACE =====
+
+void ASlot::GetOwnedGameplayTags(FGameplayTagContainer& TagContainer) const
+{
+	TagContainer = GameplayTags;
+}
--- GamelabSupermarket/Source/GamelabSupermarket/Project/Gameplay/Slot.h
+++ GamelabSupermarket/Source/GamelabSupermarket/Project/Gameplay/Slot.h
@@ -1,28 +1,45 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Product.h"
 #include "Interactable.h"
 #include "GameFramework/Actor.h"
+#include "GameplayTagAssetInterface.h"
+#include "GameplayTagContainer.h"
 #include "Slot.generated.h"
 
 UCLASS()
-class GAMELABSUPERMARKET_API ASlot : public AActor, public IInteractable
+class GAMELABSUPERMARKET_API ASlot : public AActor, public IInteractable, public IGameplayTagAssetInterface
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
+
+public:
 	ASlot();
+
 	virtual void Tick(float DeltaTime) override;
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	// Evento Blueprint
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
 	void Interact(ACharacter* Interactor);
 
+public:
+
+	// Container interno dei Gameplay Tags
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gameplay Tags")
+	FGameplayTagContainer GameplayTags;
+
+	// ===== FUNZIONI PER BLUEPRINT =====
+
+	UFUNCTION(BlueprintCallable, Category = "Gameplay Tags")
+	void AddSlotTag(FGameplayTag Tag);
+
+	UFUNCTION(BlueprintCallable, Category = "Gameplay Tags")
+	void RemoveSlotTag(FGameplayTag Tag);
+
+	// ===== INTERFACCIA EQS =====
+
+	virtual void GetOwnedGameplayTags(FGameplayTagContainer& TagContainer) const override;
 };
